5b81142438ec07bd88bc01140c784d1ec4ed759d
worgflores <worgflores@gmail.com>
2019-02-13 18:31:55 +0800

    added

---
+++ client/rameses-gov-ovs/src/com/rameses/gov/etracs/ovs/models/OVSCashReceipt.groovy
@@ -0,0 +1,62 @@
+package com.rameses.gov.etracs.ovs.models;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*
+import com.rameses.osiris2.common.*
+import com.rameses.enterprise.treasury.cashreceipt.*;
+import com.rameses.util.*;
+
+public class OVSCashReceipt extends BasicCashReceipt {
+    
+     @Service("OVSCashReceiptService")
+     def cashreceiptSvc;
+    
+     //we specify this so print detail will appear.
+     String entityName = "misc_cashreceipt";
+     String title = "Ordinance Violation";
+     def selectedItem;
+     def status;
+     
+     def payerChanged(def o){
+        entity.billitems = cashreceiptSvc.getUnpaidViolations([objid:o.objid]);
+        reloadListModel( entity.billitems.size() - 1  );
+        itemListHandler.reload();
+        updateBalances();
+        return null;
+     }
+    
+    def itemListHandler = [
+        fetchList : {o ->
+            return entity.billitems;
+        },
+        afterColumnUpdate : {o,colname ->
+            reloadListModel(status.index);
+        }
+    ] as EditorListModel;
+
+    void reloadListModel(def index){
+        entity.items.clear();
+        entity.billitems.eachWithIndex { o, idx->
+             if(idx <= index){
+                if( idx < index ) {
+                    o.checked = true;
+                }
+                if(o.checked ) {
+                    o.amount = o.balance;
+                    entity.items << [item:o.item, amount: o.amount, remarks: o.remarks, refid: o.refid, ticketid: o.ticketid ];
+                }
+                else {
+                    o.amount = 0;
+                }
+             }
+             else{
+                o.checked = false;
+                o.amount = 0.0;
+             }
+        }
+        entity.amount = entity.items.sum{it.amount};
+        updateBalances();
+    }
+   
+}
